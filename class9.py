f628ff86caa4ac48bf37da47275a8a4ce5e8bc21
Ribeiro Filho <ribeiro.pdf@hotmail.com>
2023-02-02 18:15:37 -0300

    Fixing class9

--- class9.py
+++ class9.py
@@ -1,15 +1,17 @@
 secret_word = "mandalorian"
-blanket_secret_word = list(secret_word)
+blanket_secret_word_list = list(secret_word)
 tries =  0
 
-for ind, val in enumerate(blanket_secret_word):
+for ind, val in enumerate(secret_word):
     
-    blanket_secret_word[ind] = "_"
+    blanket_secret_word_list[ind] = "_"
+
+blanket_secret_word = "".join(blanket_secret_word_list)
 
 while blanket_secret_word != secret_word:
 
     print()
-    print(f"Actual word: {blanket_secret_word}")
+    print(f"Actual word: {blanket_secret_word_list}")
     print(f"Tries: {tries}")
 
     letter_turn = input("Inform a letter: ").lower()
@@ -24,7 +26,8 @@ while blanket_secret_word != secret_word:
         
         if letter == letter_turn:
 
-            blanket_secret_word[ind] = letter
+            blanket_secret_word_list[ind] = letter
 
     tries += 1
+    blanket_secret_word = "".join(blanket_secret_word_list)
 
